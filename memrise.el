9e8c4df3e95b4049afb9d336fed253a367c8a3e7
Valeriy Savchenko <Valeriy.Savchenko@roguewave.com>
2017-09-04 19:51:59 +0300

    Implement functions to parse user's courses.

--- memrise.el
+++ memrise.el
@@ -7,11 +7,36 @@
 
 (request memrise/login-url)
 
+(defstruct memrise/course name description number-of-things learned to-review difficult)
+
+(defvar memrise/json-to-field '((name 1)
+                                (description 2)
+                                (num_things 3)
+                                (learned 4)
+                                (review 5)
+                                (difficult 6)))
+
+(defun memrise/parse-course (course-alist)
+  "Converts given alist object into a 'memrise/course' object"
+  (let ((result (make-memrise/course)))
+    (mapc (lambda (pair)
+              (let ((field (car pair))
+                    (index (cadr pair)))
+                (aset result index (assoc-default field course-alist))))
+          memrise/json-to-field)
+    result))
+
+(defun memrise/parse-courses (courses-list)
+  "Returns a list of 'memrise/course' corresponding to the given list"
+  (mapcar 'memrise/parse-course (assoc-default 'courses courses-list)))
+
 (defun memrise/get-csrf-token ()
   "Returns CSRF token for memrise.com"
-  (cdr (assoc "csrftoken" (request-cookie-alist "www.memrise.com" "/"))))
+  (assoc-default "csrftoken" (request-cookie-alist "www.memrise.com" "/")))
 
 (memrise/get-csrf-token)
+
+(memrise/parse-courses memrise/test)
 
 (defun memrise/to-buffer (buffer)
   (interactive)
@@ -37,9 +62,11 @@
  :params '(("with_num_ready_to_water" . "true"))
  :parser (memrise/to-buffer "response.json"))
 
-
 (request
  memrise/dasboard-url
  :type "GET"
  :params '(("courses_filter" . "most_recent"))
- :parser 'json-read)
+ :parser 'json-read
+ :success (cl-function
+           (lambda (&key data &allow-other-keys)
+             (message "I got: %S" (memrise/parse-courses data)))))
