8e3b0c4d993c867d4943335478bc53356073a17d
Valeriy Savchenko <sinmipt@gmail.com>
2019-08-02 01:02:22 +0300

    Document things from memrise-dashboard
    Continues on issue #3

--- memrise-dashboard.el
+++ memrise-dashboard.el
@@ -1,3 +1,32 @@
+;;; memrise-dashboard.el --- Memrise dashboard visualization and logic -*- lexical-binding: t -*-
+
+;; Copyright (C) 2017-2019  Valeriy Savchenko
+
+;; Author: Valeriy Savchenko <sinmipt@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; memrise.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; memrise.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with memrise.el.
+;; If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module contains all the functionality and logic related to the
+;; Memrise dashboard.
+
+;;; Code:
+
 (require 'memrise-request)
 (require 'memrise-session)
 (require 'memrise-ui)
@@ -12,56 +41,81 @@
     (define-key map "r" 'memrise-dashboard-course-review)
     (define-key map "g" 'memrise-dashboard)
     map)
-  "Keymap for a memrise dashboard buffer")
+  "Keymap for a memrise dashboard buffer.")
 
-(define-derived-mode memrise-mode special-mode "Memrise")
+(define-derived-mode memrise-mode special-mode "Memrise"
+  "Mode for holding Memrise dashboards")
 
 (defun memrise-dashboard-buffer ()
-  (get-buffer-create "Memrise"))
+  "Get buffer for the dashboard (it should always be no more than one)."
+  (get-buffer-create "*dashboard*"))
 
 ;;;###autoload
 (defun memrise-dashboard ()
+  "Shpw your Memrise dashboard in the *dashboard* buffer.
+
+This function is the main entry point to memrise.el.  If the user is not
+logged in it will ask for credentials.
+
+The content of the dashboard includes all of the courses *attended* by the user.
+Each course includes a number of words learned, a number of words for review, and
+a number of difficult words.  Additionally it includes a short description of the
+course.
+
+The user can jump between all of her courses and start either learning or review
+sessions."
   (interactive)
   (let ((buffer (memrise-dashboard-buffer)))
     (memrise-request-dashboard
      (lambda (data)
        (with-current-buffer buffer
+         ;; it is a read-only buffer
          (let ((inhibit-read-only t))
+           ;; typical new buffer bolierplate code
            (kill-all-local-variables)
            (erase-buffer)
            (memrise-mode)
+           ;; make a buffer-local variable containing all the courses
            (make-local-variable 'courses)
+           ;; parse the courses from the JSON data received from Memrise
            (setq courses (memrise-parse-courses data))
+           ;; display it in the buffer
            (memrise-display-courses courses buffer)
            (switch-to-buffer buffer)))))))
 
-;; Course structs
 (jeison-defclass memrise-course nil
-  ((name :documentation "name (Swedish 3)")
-   (description :documentation "description (couple of sentences)")
-   (number-of-things :path num_things :documentation "overall number of things to learn")
-   (learned :documentation "number of things already learned")
-   (to-review :path review :documentation "number of things to review/water")
-   (difficult :documentation "number of difficult words")
-   (id :documentation "memrise ID of the course")
-   (start :documentation "dashboard position where course widget starts")
-   (end :documentation "dashboard position where course widget ends")))
+  ((name :documentation "The name of the course (i.e. Swedish 3)")
+   (description
+    :documentation "The course's description (a couple of sentences)")
+   (number-of-things :path num_things
+                     :documentation "Overall number of things to learn")
+   (learned :documentation "A number of things already learned")
+   (to-review :path review :documentation "A number of things to review/water")
+   (difficult :documentation "A number of difficult words")
+   (id :documentation "Memrise ID of the course")
+   (start :documentation "Dashboard position where the course widget starts")
+   (end :documentation "Dashboard position where the course widget ends"))
+  :documentation "A class containing all the information on the course.")
 
 (defun memrise-courses ()
+  "Return a list of studied courses."
   (with-current-buffer (memrise-dashboard-buffer)
     courses))
 
 (defun memrise-current-course ()
+  "Get the course from under the point."
   (get-text-property (point-marker) 'course))
 
 (defun memrise-next-course (course courses)
+  "Return the next (with respect to the `COURSE') from the `COURSES'."
   (let ((next-courses (cdr (member course (memrise-courses)))))
     (if next-courses
         (car next-courses)
+      ;; if it was the last element - return the very first course
       (car courses))))
 
 (defun memrise-dashboard-course-learn (course)
-  "Starts 'learning new words' session for the given course"
+  "Start 'learning new words' session for the given `COURSE'."
   (interactive (list (memrise-current-course)))
   (let ((all (oref course number-of-things))
         (learned (oref course learned)))
@@ -70,7 +124,7 @@
       (memrise-start-learn-session (oref course id)))))
 
 (defun memrise-dashboard-course-review (course)
-  "Starts review/water session for the given course"
+  "Start review/water session for the given `COURSE'."
   (interactive (list (memrise-current-course)))
   (let ((to-review (oref course to-review)))
     (if (eq to-review 0)
@@ -78,18 +132,19 @@
       (memrise-start-review-session (oref course id)))))
 
 (defun memrise-dashboard-course-forward (course)
-  "Moves cursor to a next course on the dashboard"
+  "Move cursor to the next (with respect to the `COURSE') on the dashboard."
   (interactive (list (memrise-current-course)))
   (let ((dest (memrise-next-course course courses)))
     (goto-char (oref dest start))))
 
 (defun memrise-dashboard-course-backward (course)
-  "Moves cursor to a previous course on the dashboard"
+  "Move cursor to a previous (with respect to the `COURSE') on the dashboard."
   (interactive (list (memrise-current-course)))
   (let ((dest (memrise-next-course course (reverse courses))))
     (goto-char (oref dest start))))
 
 (defun memrise-display-courses (courses buffer)
+  "Display `COURSES' in the `BUFFER'."
   (with-current-buffer buffer
     (mapc 'memrise-display-course courses)))
 
@@ -109,35 +164,43 @@
 
 (defcustom memrise-review-icon
   (all-the-icons-faicon "tint" :v-adjust 0.0)
-  "Icon to use for words that require review/water"
+  "Icon to use for words that require review/water."
   :type 'string
   :group 'memrise)
 
 (defcustom memrise-difficult-icon
   (all-the-icons-faicon "bolt" :v-adjust 0.0)
-  "Icon to use for words marked as 'difficult'"
+  "Icon to use for words marked as 'difficult'."
   :type 'string
   :group 'memrise)
 
+(defun memrise-display-course (course)
+  "Display `COURSE' in the current buffer."
+  ;; TODO: it is a good place to use EIEIO `with-slots'
+  (let ((format-objects `((name . ,(oref course name))
+                          (learned . ,(oref course learned))
+                          (all . ,(oref course number-of-things))
+                          (rev-icon . ,memrise-review-icon)
+                          (review . ,(oref course to-review))
+                          (diff-icon . ,memrise-difficult-icon)
+                          (difficult . ,(oref course difficult))
+                          (description . ,(oref course description)))))
+    (memrise-insert-course
+     (memrise-format-elements-with-faces memrise-dashboard-format
+                                         format-objects
+                                         memrise-dashboard-faces)
+     course)
+    (insert "\n\n")))
+
 (defun memrise-insert-course (text course)
+  "Insert `TEXT' and mark it as `COURSE'."
   (oset course start (point-marker))
   (insert (propertize text 'course course))
   (oset course end (point-marker)))
 
-(defun memrise-display-course (course)
-  (let* ((format-objects `((name . ,(oref course name))
-                           (learned . ,(oref course learned))
-                           (all . ,(oref course number-of-things))
-                           (rev-icon . ,memrise-review-icon)
-                           (review . ,(oref course to-review))
-                           (diff-icon . ,memrise-difficult-icon)
-                           (difficult . ,(oref course difficult))
-                           (description . ,(oref course description)))))
-    (memrise-insert-course (memrise-format-elements-with-faces memrise-dashboard-format format-objects memrise-dashboard-faces) course)
-    (insert "\n\n")))
-
 (defun memrise-parse-courses (courses-list)
-  "Returns a list of 'memrise-course' corresponding to the given list"
+  "Return a list of `memrise-course' corresponding to the `COURSES-LIST'."
   (jeison-read '(list-of memrise-course) courses-list 'courses))
 
 (provide 'memrise-dashboard)
+;;; memrise-dashboard.el ends here
